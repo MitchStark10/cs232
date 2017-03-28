09018675d2e729a09da6f8430a851406bf8fad4f
Mitchell Stark <mitchstark@Mitchells-MacBook-Pro.local>
2017-03-28 19:47:15 -0400

    most of the way there, needs testing on linux

--- proj4/bakeshop.c
+++ proj4/bakeshop.c
@@ -6,31 +6,66 @@
 // A normal C function that is executed as a thread when its name
 // is specified in pthread_create()
 sem_t customerSemaphore;
+sem_t bakerSemaphore;
 int customers = 0;
-
+bool customerWaiting = false;
+int breadAvailable = 0;
 
 void *customerActions(void *vargp)
 {
-    sem_close(&customerSemaphore);
-    sleep(1);
-    printf("Printing hello from Thread \n");
-    printf(customers);
-    printf("\n");
+    sem_wait(&customerSemaphore);
+    //CRITICAL SECTION
+    printf("Printing hello from customer\n");
+
+    //request bread
+    while(breadAvailable == 0) {
+        printf("Customer waiting for bread...");
+    }
+
+    breadAvailable--;
+    printf("Customer received bread!");
+    //pay
+    bakerCheckout();
+    //Finish the crtical section
     customers--;
     sem_post(&customerSemaphore);
     return NULL;
 }
 
+void bakeBread() {
+    while(customers != 0) {
+        sem_wait(&bakerSemaphore);
+        //CRITICAL SECTION
+        breadAvailable++;
+        printf("Baker just baked a single loaf of bread");
+        sem_post(&bakerSemaphore);
+    }
+    printf("Baker is completely done baking loaves of bread.");
+}
+
+void bakerCheckout() {
+    printf("Customer waiting to checkout...");
+    sem_wait(&bakerSemaphore);
+    //CRITICAL SECTION
+    printf("Thread has just checked out!");
+    sem_post(&bakerSemaphore);
+}
+
 int main()
 {
-    sem_init(&customerSemaphore, 0, 1);
+    initSemaphores();
     printf("---Creating Threads---\n");
     for(int i = 0; i < 10; i++) {
-        customers++;
         pthread_t tid;
         pthread_create(&tid, NULL, customerActions, NULL);
     }
     pthread_exit(NULL);
     printf("---All customers have finished---\n");
     exit(0);
+}
+
+
+void initSemaphores() {
+    sem_init(&customerSemaphore, 0, 1);
+    sem_init(&bakerSemaphore, 0, 1);
 }
