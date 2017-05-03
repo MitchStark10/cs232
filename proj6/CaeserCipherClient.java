8cb8a304bfd2a15db37b9f94b92dc001b43ba351
MitchStark10 <mitch.stark.github@gmail.com>
2017-05-03 09:56:55 -0400

    created caesar cipher client java class

---
+++ proj6/CaeserCipherClient.java
@@ -0,0 +1,20 @@
+/*
+Created by: Mitch Stark
+Date: 5/3/17
+Purpose: Client class for project 6 CS 232
+*/
+
+import java.util.Scanner;
+
+public class CaeserCipherClient {
+    //client start point
+    public static void main(String[] args) {
+        System.out.println("Welcome to the CaeserCipher service. Please enter" +
+            " the offset integer: ");
+
+        Scanner s = new Scanner(System.in);
+        Integer offset = s.nextInt();
+
+    }
+
+}
